a6540260b966c4df3cc2adf1dc68d22dff5e7133
Makoto Shimizu <mak@fish.razor.jp>
2022-06-22 00:06:03 +0900

    install ga4

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name='megaton',
+    version='0.0.1',
+    author='Mak Shimizu',
+    author_email='mak@fish.razor.jp',
+    description='Utilities for Google Analytics, Google Analytics 4 and Google Cloud Platform.',
+    long_description=readme,
+    long_description_content_type='ext/markdown',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+    ],
+    python_requires='>=3.7',
+)
