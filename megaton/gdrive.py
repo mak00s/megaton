181cf0228f67457459f7261a142141f5d1183c9b
Makoto Shimizu <mak@fish.razor.jp>
2022-06-22 09:44:45 +0900

    gdrive

---
+++ megaton/gdrive.py
@@ -0,0 +1,20 @@
+import sys
+import os
+from pathlib import Path
+
+from google.colab.drive import mount
+
+
+def link_nbs():
+    """ Create /nbs as a symlink to "Colab Notebooks".
+    Also add /nbs to python path, so we can store libraries there
+    Allow import A to get A.ipybn as well
+    """
+    if not Path("/content/drive").exists():
+        mount("/content/drive")
+    # create link if not already
+    if not Path("/nbs").exists():
+        os.symlink("/content/drive/My Drive/Colab Notebooks", "/nbs")
+    # add path if not already
+    if '/nbs' not in sys.path:
+        sys.path.insert(5, '/nbs')  # before dist-packages
