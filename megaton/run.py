7bf36cc8aee0d5099d89426dac66cac5a96a4273
Makoto Shimizu <mak@fish.razor.jp>
2022-06-22 21:25:55 +0900

    auth

--- megaton/run.py
+++ megaton/run.py
@@ -4,23 +4,25 @@ from IPython.display import clear_output
 import os
 from time import sleep
 
-from . import gdrive
+from . import auth, gdrive
 
 
-# install packages for GA4
 try:
+    # check if packages for GA4 are installed
     from google.analytics.data import BetaAnalyticsDataClient
 except ModuleNotFoundError:
-    print("Installing packages for GA4")
+    print("Installing packages for GA4...")
     from .install import ga4
-    from .update import google_api_core
-
     clear_output()
     # print("Runtime is now restarting...")
     # print("You can ignore the error message [Your session crashed for an unknown reason.]")
     print("もう一度このセルを実行してください。")
     sleep(0.5)
     os._exit(0)  # restart
-else:
-    gdrive.link_nbs()
-    print("準備ができました。")
+
+# mount google drive
+gdrive.link_nbs()
+
+json_files = auth.get_client_secrets_from_dir('/nbs')
+
+print("準備ができました。")
--- megaton/update/google_api_core.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# update packages
-os.system("pip install -U -q google-api-core")
