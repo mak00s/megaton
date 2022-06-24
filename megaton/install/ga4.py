c9e411837eb2b3c6b50070c484f5718d749ab236
Makoto Shimizu <mak@fish.razor.jp>
2022-06-25 00:47:54 +0900

    fix

--- megaton/install/ga4.py
+++ megaton/install/ga4.py
@@ -1,12 +1,7 @@
 import os
 
-try:
-    # check if packages for GA4 are installed
-    from google.analytics.admin import AnalyticsAdminServiceClient
-    from google.analytics.data import BetaAnalyticsDataClient
-except ModuleNotFoundError:
-    # install packages for GA4
-    os.system("pip install -U -q google-analytics-admin")
-    os.system("pip install -U -q google-analytics-data")
-    # update packages
-    os.system("pip install -U -q google-api-core")
+# install packages for GA4
+os.system("pip install -U -q google-analytics-admin")
+os.system("pip install -U -q google-analytics-data")
+# update packages
+os.system("pip install -U -q google-api-core")
