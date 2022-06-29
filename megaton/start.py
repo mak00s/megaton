5ab35b38f4859c55541460ebb1539f942a19859f
Makoto Shimizu <mak@fish.razor.jp>
2022-06-29 23:26:04 +0900

    added show method

--- megaton/start.py
+++ megaton/start.py
@@ -28,6 +28,7 @@ from . import auth, constants, errors, ga3, ga4, widgets
 
 _in_colab = "google.colab" in sys.modules
 if _in_colab:
+    from google.colab import data_table
     # mount google drive
     from . import gdrive
     json_path = gdrive.link_nbs()
@@ -40,17 +41,17 @@ logger.debug("Notebookの準備ができました。")
 class Megaton:
     """ メガトンはGAを使うアナリストの味方
     """
-
     def __init__(self, path: str = None, use_ga3: bool = True):
         if not path and _in_colab:
             path = '/nbs'
-        self.use_ga3 = use_ga3
         self.json = None
         self.creds = None
         self.auth_menu = None
+        self.use_ga3 = use_ga3
         self.ga = {}  # GA clients
         self.ga_ver = None
         self.select = self.Select(self)
+        self.show = self.Show(self)
 
         self.auth(path)
 
@@ -101,39 +102,62 @@ class Megaton:
         self.auth_menu.reset()
         self.select.reset()
 
-    class Select:
-        """ 選択するUIの構築と処理
-        """
-        def __init__(self, parent):
+    class AuthMenu:
+        def __init__(self, parent, json_files: dict):
             self.parent = parent
-            self.menu_ga = {}
+            # build menu of json files
+            self.creds_selector = widgets.menu_for_credentials(json_files)
+            self.log_text = widgets.Output()
+            self.message_text = widgets.html_text(value='', placeholder='', description='')
+            self.code_selector = widgets.input_text(
+                value='',
+                placeholder='コードをここへ',
+                description='CODE:',
+            )
+            self.flow = None
+
+        # when json is selected
+        def _json_selected(self, change):
+            with self.log_text:
+                self.parent.reset_menu()
+                if change.new:
+                    creds_type = auth.get_client_secrets_type_from_file(change.new)
+                    if creds_type in ['installed', 'web']:
+                        self.flow, auth_url = auth.get_oauth_redirect(change.new, constants.DEFAULT_SCOPES)
+                        self.message_text.value = f'<a href="{auth_url}" target="_blank">ここをクリックし、認証後に表示されるcodeを以下に貼り付けてエンターを押してください</a>'
+                        self.code_selector.layout.display = "block"
+
+                    if creds_type in ['service_account']:
+                        self.parent.creds = auth.load_service_account_credentials_from_file(change.new,
+                                                                                            constants.DEFAULT_SCOPES)
+                        self.parent.json = change.new
+                        self.parent.select.ga()
+
+        # when code is entered
+        def _code_entered(self, change):
+            with self.log_text:
+                if change.new:
+                    try:
+                        self.parent.creds = auth.get_token(self.flow, change.new)
+                        self.code_selector.value = ''
+                        self.code_selector.layout.display = "none"
+                        self.parent.json = change.new
+                        self.parent.select.ga()
+                        self.reset()
+                    except InvalidGrantError:
+                        logging.error("正しいフォーマットのauthorization codeを貼り付けてください")
+
+        def show(self):
+            self.reset()
+            self.creds_selector.observe(self._json_selected, names='value')
+            self.code_selector.observe(self._code_entered, names='value')
+            display(self.creds_selector, self.message_text, self.code_selector, self.log_text)
 
         def reset(self):
-            if '3' in self.menu_ga.keys():
-                self.menu_ga['3'].reset()
-            if '4' in self.menu_ga.keys():
-                self.menu_ga['4'].reset()
-
-        def ga(self):
-            """ GAアカウントを選択するパネルを表示
-            """
-            # 選択された認証情報でGAクライアントを生成
-            self.parent.build_ga_clients()
-            # メニューをリセット
-            self.reset()
-
-            # GA選択メニューを表示
-            for ver in ['3', '4']:
-                if ver in self.parent.ga.keys():
-                    try:
-                        self.menu_ga[ver] = self.parent.GaMenu(self.parent, ver, self.parent.ga[ver].accounts)
-                        self.menu_ga[ver].show()
-                    except errors.NoDataReturned:
-                        logger.warning("選択された認証情報でアクセスできるアカウントがありません")
-                        del self.ga[ver]
-                    except errors.ApiDisabled as e:
-                        logger.warning(f"GCPプロジェクトで{e.api}を有効化してください")
-                        del self.parent.ga[ver]
+            self.message_text.value = ''
+            self.code_selector.layout.display = "none"
+            self.code_selector.value = ''
+            self.log_text.value = ''
 
     class GaMenu:
         """ GAのアカウント・プロパティ（・ビュー）を選択するUI
@@ -224,59 +248,78 @@ class Megaton:
             if self.ver == '3':
                 self.view_menu.options = []
 
-    class AuthMenu:
-        def __init__(self, parent, json_files: dict):
+    class Select:
+        """ 選択するUIの構築と処理
+        """
+        def __init__(self, parent):
             self.parent = parent
-            # build menu of json files
-            self.creds_selector = widgets.menu_for_credentials(json_files)
-            self.log_text = widgets.Output()
-            self.message_text = widgets.html_text(value='', placeholder='', description='')
-            self.code_selector = widgets.input_text(
-                value='',
-                placeholder='コードをここへ',
-                description='CODE:',
-            )
-            self.flow = None
-
-        # when json is selected
-        def _json_selected(self, change):
-            with self.log_text:
-                self.parent.reset_menu()
-                if change.new:
-                    creds_type = auth.get_client_secrets_type_from_file(change.new)
-                    if creds_type in ['installed', 'web']:
-                        self.flow, auth_url = auth.get_oauth_redirect(change.new, constants.DEFAULT_SCOPES)
-                        self.message_text.value = f'<a href="{auth_url}" target="_blank">ここをクリックし、認証後に表示されるcodeを以下に貼り付けてエンターを押してください</a>'
-                        self.code_selector.layout.display = "block"
-
-                    if creds_type in ['service_account']:
-                        self.parent.creds = auth.load_service_account_credentials_from_file(change.new,
-                                                                                            constants.DEFAULT_SCOPES)
-                        self.parent.json = change.new
-                        self.parent.select.ga()
-
-        # when code is entered
-        def _code_entered(self, change):
-            with self.log_text:
-                if change.new:
-                    try:
-                        self.parent.creds = auth.get_token(self.flow, change.new)
-                        self.code_selector.value = ''
-                        self.code_selector.layout.display = "none"
-                        self.parent.json = change.new
-                        self.parent.select.ga()
-                        self.reset()
-                    except InvalidGrantError:
-                        logging.error("正しいフォーマットのauthorization codeを貼り付けてください")
-
-        def show(self):
-            self.reset()
-            self.creds_selector.observe(self._json_selected, names='value')
-            self.code_selector.observe(self._code_entered, names='value')
-            display(self.creds_selector, self.message_text, self.code_selector, self.log_text)
+            self.menu_ga = {}
 
         def reset(self):
-            self.message_text.value = ''
-            self.code_selector.layout.display = "none"
-            self.code_selector.value = ''
-            self.log_text.value = ''
+            if '3' in self.menu_ga.keys():
+                self.menu_ga['3'].reset()
+            if '4' in self.menu_ga.keys():
+                self.menu_ga['4'].reset()
+
+        def ga(self):
+            """ GAアカウントを選択するパネルを表示
+            """
+            # 選択された認証情報でGAクライアントを生成
+            self.parent.build_ga_clients()
+            # メニューをリセット
+            self.reset()
+
+            # GA選択メニューを表示
+            for ver in ['3', '4']:
+                if ver in self.parent.ga.keys():
+                    try:
+                        self.menu_ga[ver] = self.parent.GaMenu(self.parent, ver, self.parent.ga[ver].accounts)
+                        self.menu_ga[ver].show()
+                    except errors.NoDataReturned:
+                        logger.warning("選択された認証情報でアクセスできるアカウントがありません")
+                        del self.ga[ver]
+                    except errors.ApiDisabled as e:
+                        logger.warning(f"GCPプロジェクトで{e.api}を有効化してください")
+                        del self.parent.ga[ver]
+
+    class Show:
+        def __init__(self, parent):
+            self.parent = parent
+
+        @property
+        def ga(self):
+            return self.Ga(self)
+
+        class Ga:
+            def __init__(self, parent):
+                self.parent = parent
+
+            @property
+            def dimensions(self):
+                print("dimensions are:")
+                df = self.parent.parent.ga['4'].property.show('dimensions')
+                self.parent.table(df)
+
+            @property
+            def metrics(self):
+                print("metrics are:")
+                df = self.parent.parent.ga['4'].property.show('metrics')
+                self.parent.table(df)
+
+            @property
+            def properties(self):
+                print("properties are:")
+                df = self.parent.parent.ga['4'].property.show('info')
+                self.parent.table(df)
+
+        def table(self, df, rows: int = 10, include_index: bool = False):
+            if _in_colab:
+                return data_table.DataTable(
+                    df,
+                    include_index=include_index,
+                    num_rows_per_page=rows
+                )
+            try:
+                itables.show(df)
+            except NameError:
+                display(df)
