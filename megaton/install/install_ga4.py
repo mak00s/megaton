6dce87ce9688b640db5cdcd6965777a1775345b8
Makoto Shimizu <mak@fish.razor.jp>
2024-02-05 20:44:43 +0900

    bump google-auth

--- megaton/install/install_ga4.py
+++ megaton/install/install_ga4.py
@@ -5,3 +5,4 @@ os.system("pip install -U -q google-analytics-admin")
 os.system("pip install -U -q google-analytics-data")
 # update packages
 # os.system("pip install -U -q google-api-core")
+os.system("pip install -U -q google-auth")
